99b041a5862681e15361ea6e0d956f9b8499a51c
John Sim <jsim@fishbowlsolutions.com>
2022-09-16 14:22:32 +0100

    Create Base for sample OCM Custom Form

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '$lib/styles/normalise.css';
+	import '$lib/styles/index.scss';
+</script>
+
+<div class="container">
+	<slot />
+</div>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: $spacing-4 $spacing-0 $spacing-0;
+		margin: $spacing-0 auto;
+		min-height: 100vh;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<main>
+	Base Empty Sveltekit project for OCM PWA Custom Form Example
+</main>
