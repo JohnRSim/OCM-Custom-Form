df54a257f6ab0f2552f8222845bb17ffa00d6b2f
John Sim <jsim@fishbowlsolutions.com>
2022-09-18 12:51:14 +0100

    add default value field for lang support

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -1,10 +1,12 @@
 <script>
+	export let pattern = false;
 	export let fieldName = 'unknown';
 	export let type = 'Single text box';
 	export let placeholder = 'Enter Text';
 	export let items = [];
 	export let focus=false;
-	
+	export let defaultValue;
+
 	placeholder = ((placeholder) && (placeholder.length > 0))?placeholder:'Enter Text';
 </script>
 
@@ -28,19 +30,19 @@
 
 {#if (type==="Single-select menu")}
 	<select
+		bind:value={defaultValue}
 		class="
-		block
-		w-full
-		mt-1
-		rounded-md
-		border-gray-300
-		shadow-sm
-		focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-	"
-	>
-	{#each items as item}
-		<option value="{item.value}">{item.label}</option>
-	{/each}
+			block
+			w-full
+			mt-1
+			rounded-md
+			border-gray-300
+			shadow-sm
+			focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+		">
+		{#each items as item}
+			<option value="{item.value}" >{item.label}</option>
+		{/each}
 	</select>
 {/if}
 <style lang="scss">
