3184f1461b8843869d49fd8acb2f8993a2627f2f
John Sim <jsim@fishbowlsolutions.com>
2022-09-18 12:50:57 +0100

    enable collapse

--- src/lib/components/Group.svelte
+++ src/lib/components/Group.svelte
@@ -3,7 +3,7 @@
 
 	export let isCard = false;
 	export let title = false;
-	export let showContent = true;
+	export let collapse = false;
 
 	onMount(() => {
 
@@ -14,16 +14,19 @@
 	 * show hide content region
 	 */
 	function toggle() {
-		showContent = !showContent;
+		collapse = !collapse;
 	}
 </script>
 
-<dl class:show="{showContent}" class="{(isCard)?'shadow-md rounded-xl mt-4 mb-10 bg-white ring-1 ring-slate-700/10':''}">
+<!-- Group -->
+<dl class:show="{!collapse}" class="{(isCard)?'shadow-md rounded-xl mt-4 mb-10 bg-white ring-1 ring-slate-700/10':''}">
 	{#if (title)}
+		<!-- Title -->
 		<dt on:click="{toggle}" name="title" class="cursor-pointer relative py-4 pl-4 pr-10 font-semibold border-b border-grey">
 			{title}
+			<!-- Toggle Collapse -->
 			<i class="absolute top-4 right-4">		
-				{#if (showContent)}
+				{#if (!collapse)}
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="w-6 h-6">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"></path>
 					</svg>
@@ -33,18 +36,20 @@
 					</svg>
 				{/if}
 			</i>
-
+			<!-- xToggle Collapse -->
 		</dt>
+		<!-- xTitle -->
 	{/if}
+
+	<!-- Content -->
 	<dd class:p-4="{isCard}" class:mt-8="{!isCard}" class:p-1="{!isCard}">
 		<slot name="content"></slot>
 	</dd>
+	<!-- xContent -->
 </dl>
+<!-- xGroup -->
 
 <style lang="scss">
-	dt {
-
-	}
 	dd {
 		opacity:0;
 		height:0px;
